4999bb41fc93826669e788863e74b0e2b2e7ba8c
Michael <mixkok2001@gmail.com>
2025-11-22 21:27:54 +0200

    Small ui fix for languages selection

--- src/components/SimpleRadioButton.tsx
+++ src/components/SimpleRadioButton.tsx
@@ -19,28 +19,36 @@ const RadioButton: React.FC<LanguageRadioProps> = ({
   onChange,
 }) => {
   return (
-    <div className="flex gap-4 items-center">
+    <div className="flex flex-col justify-start gap-4 items-start">
+      <span className="text-2xl ml-1">Languages</span>
       {options.map((opt: LanguageOption) => (
-        <label
-          key={opt.language}
-          className="flex items-center gap-2 cursor-pointer p-2"
-        >
-          <input
-            type="radio"
-            name="language"
-            value={opt.language}
-            checked={selected === opt.language}
-            onChange={() => onChange(opt.language)}
-            className="hidden"
-          />
+        <label key={opt.language} className="cursor-pointer w-full">
+          <div className="w-38 flex items-center ps-4 px-4 rounded-xl border border-gray-700 bg-neutral-primary-soft rounded-base">
+            <input
+              type="radio"
+              name="language"
+              value={opt.language}
+              checked={selected === opt.language}
+              onChange={() => onChange(opt.language)}
+              className=""
+            />
+            <span className="ml-3 text-2xl">{opt.flag}</span>
+            <span
+              className="w-full py-4 select-none ms-2 text-sm font-medium text-heading"
+            >
+              {opt.language}
+            </span>
+          </div>
 
-          <motion.div
+          {/* <motion.div
             animate={{ scale: selected === opt.language ? 1.1 : 1 }}
-            className="flex items-center gap-2"
+            className=""
           >
-            <span className="text-2xl">{opt.flag}</span>
-            <span className="text-base">{opt.language}</span>
-          </motion.div>
+            <div className="flex gap-1 items-center outline outline-gray-500 px-4 py-2">
+              <span className="text-2xl">{opt.flag}</span>
+              <span className="text-base">{opt.language}</span>
+            </div>
+          </motion.div> */}
         </label>
       ))}
     </div>
